e5ed1d47f08b0b4cbaf41ca7b96c6a81ae34bd89
NastiaCandor <andubova98@gmail.com>
2023-04-15 23:00:48 +0300

    feat: done stack task

--- src/stack.js
+++ src/stack.js
@@ -1,29 +1,15 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
- */
 class Stack {
   constructor() {
     this.data = [];
   }
   
   push(element) {
-    // throw new NotImplementedError('Not implemented');
     this.data.push(element);
   }
 
   pop() {
-    // throw new NotImplementedError('Not implemented');
     let item = this.data[this.data.length -1];
     this.data.pop();
     return item;
@@ -31,7 +17,6 @@ class Stack {
   }
 
   peek() {
-    // throw new NotImplementedError('Not implemented');
     return this.data[this.data.length -1];
   }
 }
